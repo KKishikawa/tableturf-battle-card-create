60fc70d7c0ea85042d94d0a364488f706a62b52d
KKishikawa <k1saku.philosophyz@gmail.com>
2022-10-10 21:37:15 +0900

    [add]数値変換のテストパターンに境界値を追加

--- tests/utils/convert.spec.ts
+++ tests/utils/convert.spec.ts
@@ -8,8 +8,8 @@ describe("toInt", function () {
     expect(isNaN(convert.toInt(null, NaN))).toBe(true);
     expect(convert.toInt("", -1)).toBe(-1);
   });
-  it.each([["13", 13], ["3.8", 3], ["98", 98]])("val %p", function(input, output) {
-    expect(convert.toInt(input)).toBe(output);
+  it.each([["1", 1],["0", 0],["13", 13], ["3.8", 3], ["98", 98]])("val %p", function(input, output) {
+    expect(convert.toInt(input, -1)).toBe(output);
   });
 });
 
@@ -21,7 +21,7 @@ describe("toFloat", function () {
     expect(isNaN(convert.toFloat(null, NaN))).toBe(true);
     expect(convert.toFloat("", -1)).toBe(-1);
   });
-  it.each([["13", 13], ["3.8", 3.8], ["98.673333", 98.673333]])("val %p", function(input, output) {
-    expect(convert.toFloat(input)).toBe(output);
+  it.each([["1", 1],["0", 0],["13", 13], ["3.8", 3.8], ["98.673333", 98.673333]])("val %p", function(input, output) {
+    expect(convert.toFloat(input, -1)).toBe(output);
   });
 });
