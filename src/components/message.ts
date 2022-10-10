9e976f201b10b446b71b0bd2c298bcebc97311f2
KKishikawa <k1saku.philosophyz@gmail.com>
2022-10-10 11:42:31 +0900

    [update]クラス名の並びを修正

--- src/components/message.ts
+++ src/components/message.ts
@@ -1,5 +1,5 @@
 import { htmlToElement } from "@/utils";
-const messageBaseHtml = `<div class="flex p-3 text-sm rounded-lg mx-auto w-5/6 max-w-lg break-all" role="alert">
+const messageBaseHtml = `<div class="pointer-events-auto flex p-3 text-sm rounded-lg mx-auto w-5/6 max-w-lg break-all" role="alert">
 <div class="flex-shrink-0 w-5 h-5 mr-3 text-base flex items-center"><i aria-hidden="true"></i></div><span class="sr-only">Info</span><div class="message-text"></div>
 <button type="button" class="ml-auto -mx-1.5 -my-1.5 text-base flex-shrink-0 justify-center items-center rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8" aria-label="Close">
 <span class="sr-only">Close</span><i class="fa fa-xmark"></i></button></div>`;
@@ -47,7 +47,7 @@ class Message {
     messageBaseEl.querySelector("button")!.className += styleInfo.dismiss;
     const msgWrapper = document.createElement("div");
     msgWrapper.className =
-      "relative z-40 mt-4 w-full ease-out duration-300 transition-all max-h-full";
+      "relative z-40 w-full max-h-full mt-4 transition-all duration-300 ease-out";
     msgWrapper.append(messageBaseEl);
     this.element = msgWrapper;
 
